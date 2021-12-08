00f34f51b4ada72c320e6f2c56fbb0c8c89fe156
Ivo Georgiev <ivogeorg@gmail.com>
2021-12-08 08:50:02 -0700

    testing shutdown

--- fexecutive.py
+++ fexecutive.py
@@ -1,8 +1,13 @@
 from subprocess import run
 
 # 1. all necessary imports
+import sys
+import smbus
+import struct
+import RPi.GPIO as GPIO
 import time
 import os
+from subprocess import run
 import board
 import adafruit_bme680
 import adafruit_tmp117
@@ -18,17 +23,30 @@ from datetime import datetime
 
 
 # 2. setup all components
+GPIO_PORT = 26          # psu
+I2C_ADDR = 0x36         # psu
+
+bus = smbus.SMBus(1)    # psu
+
 file =open("/home/pi/lawn_demo/data_log.cvs","a")
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
-tmp117 = adafruit_tmp117.TMP117(i2c)
+#tmp117 = adafruit_tmp117.TMP117(i2c)
 icm = adafruit_icm20x.ICM20948(i2c)
 ina = INA219(i2c)
 
 bme680.sea_level_pressure = 1013.25
 temperature_offset = -5
+
+def read_voltage(bus):
+    address = I2C_ADDR
+    read = bus.read_word_data(address, 2)
+    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+    voltage = swapped * 1.25 /1000 /16
+    return voltage
+
 
 def cpu_temp():
     temp = os.popen("vcgencmd measure_temp").readline()
@@ -37,7 +55,10 @@ def cpu_temp():
     return (temp.replace("temp=",""))
 
 if os.stat("/home/pi/lawn_demo/data_log.cvs").st_size == 0:
-    file.write("time,to,ti,gas,hum,press,alt,accx,accy,accz,gyrox,gyroy,gyroz,magnx,magny,magnz,vbus,ibus,pbus,tc\n")
+    # file.write("time,to,ti,gas,hum,press,alt,accx,accy,accz," +
+    #     "gyrox,gyroy,gyroz,magnx,magny,magnz,vbus,ibus,pbus,tc\n")
+    file.write("time,ti,gas,hum,press,alt,accx,accy,accz," +
+        "gyrox,gyroy,gyroz,magnx,magny,magnz,vbus,ibus,pbus,tc\n")
 
 # 3. start wifi hot spot and streaming as a separate process
 # TODO: Ugh!
@@ -54,16 +75,41 @@ if os.stat("/home/pi/lawn_demo/data_log.cvs").st_size == 0:
 #        every minute (or every 6th iteration) attempt to send them over iridium
 
 for _ in range(10):
-    now = datetime.now()  # TODO: hh:mm only
-    data_points = [tmp117.temperature, bme680.temperature, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude, *icm.acceleration, *icm.gyro, *icm.magnetic, ina.bus_voltage, ina.current, ina.power]
+    # check for low voltage
+    if read_voltage(bus) < 3.00:
+        print("Low voltage (<3.00V)")
+        break
+
+    # get timestamp w/o microseconds
+    now = datetime.now()
+    now = now.replace(microsecond=0)  # truncate ms
+    now.strftime('%y-%m-%d %H:%M:&S')
+    print(now)
+
+    # get sensor data points
+    # data_points = [tmp117.temperature, bme680.temperature, bme680.gas, 
+    #     bme680.relative_humidity, bme680.pressure, bme680.altitude, 
+    #     *icm.acceleration, *icm.gyro, *icm.magnetic, ina.bus_voltage, 
+    #     ina.current, ina.power]
+    data_points = [bme680.temperature, bme680.gas, 
+        bme680.relative_humidity, bme680.pressure, bme680.altitude, 
+        *icm.acceleration, *icm.gyro, *icm.magnetic, ina.bus_voltage, 
+        ina.current, ina.power]
+
+    # truncate to 2 decimal places
     as_string = ','.join([str(round(d, 2)) for d in data_points])
-    file.write(str(now) + ',' + as_string + ',' + cpu_temp())
+
+    # compose data line
+    line = str(now) + ',' + as_string + ',' + cpu_temp() + '\n'
+    # print(len(line))  # 126 w/ tmp117
+
+    # append line to telemetry file
+    file.write(line)
     file.flush()
 
-
+# close telemetry file
 file.close()
 
-
-#
-# run("shutdown --poweroff now", shell=True)
-#
+# shut down
+print("Shutting down in 60 sec")
+run("shutdown --poweroff 1", shell=True)
