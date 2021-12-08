37a893555b6b1afa7fc7c1f08ec86c9ebfa1ddf0
Ivo Georgiev <ivogeorg@gmail.com>
2021-12-07 20:21:29 -0700

    clarification

--- fexecutive.py
+++ fexecutive.py
@@ -2,15 +2,17 @@ from subprocess import run
 
 # 1. all necessary imports
 # 2. setup all components
-# 3. start wifi hot spot and camera recording and straming as a separate process
-# 4. while loop running every 10 seconds
+# 3. start wifi hot spot and streaming as a separate process
+# 4. while loop running with 10 seconds delay after last iteration
 #        check power levels (voltage and capacity %)
 #        if low
-#            send message to iridium
-#            stop all processes (camera)
+#            attempt to send message over iridium
+#            if unsuccessful
+#                alert to streaming webpage (???)
+#            stop all spawned processes
 #            shutdown     
 #        read all telemetry values and log them
-#        every minute (or every 6th iteration) log and send them over iridium
+#        every minute (or every 6th iteration) attempt to send them over iridium
 
 
 run("shutdown --poweroff now", shell=True)
