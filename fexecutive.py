502453e51c5d363b041d771a10d63b3d122323c2
Ivo Georgiev <ivogeorg@gmail.com>
2022-01-27 22:17:19 -0700

    added gps, minor edits

--- fexecutive.py
+++ fexecutive.py
@@ -21,7 +21,7 @@ from picamera import PiCamera
 from datetime import datetime
 import serial
 from adafruit_rockblock import RockBlock
-# from gps3 import agps3   # GPS library
+from gps3 import agps3   # GPS library
 
 # 2. setup all components
 GPIO_PORT = 26          # psu
@@ -29,7 +29,7 @@ I2C_ADDR = 0x36         # psu
 
 bus = smbus.SMBus(1)    # psu
 
-TELEMETRY_FILENAME = "/home/pi/jan28_flight_prep/data_log.csv"
+TELEMETRY_FILENAME = "/home/pi/flight_telemetry/telemetry_log.csv"
 telemetry_file =open(TELEMETRY_FILENAME,"a")
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -42,33 +42,19 @@ ina = INA219(i2c)
 uart = serial.Serial("/dev/ttyUSB0", 19200)
 rb = RockBlock(uart)
 
-# # GPS Loop Start <===================
-# # GPSDSocket creates a GPSD socket connection & request-retrieves the GPSD output
-# gps_socket = agps3.GPSDSocket()
+# GPS Setup Start <====================
+# GPSDSocket creates a GPSD socket connection & request-retrieves the GPSD output
+gps_socket = agps3.GPSDSocket()
 
-# # DataStream unpacks the streamed GPSD data into Python dictionaries
-# data_stream = agps3.DataStream()
-# gps_socket.connect()
-# gps_socket.watch()
+# DataStream unpacks the streamed GPSD data into Python dictionaries
+data_stream = agps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
 
-# gps_lat = -360.0
-# gps_lon = -360.0
-# gps_alt = -100.0
-# # TODO: What exactly is returned? What's in the dictionaries? Is this an infinite loop
-# for new_data in gps_socket:
-#     if new_data:
-#         data_stream.unpack(new_data)
-#         # WGS84 coordinates
-#         gps_lat = data_stream.lat
-#         gps_lon = data_stream.lon
-#         gps_alt = data_stream.alt
-#         if gps_lat != 'n/a' and gps_lon != 'n/a':
-#             print(gps_lon, gps_lat, gps_alt)
-#         else:
-#             print("No GPS data. Are you indoors? :D")
-#             break
-# # GPS Loop End <===================
-
+gps_lat = -360.0
+gps_lon = -360.0
+gps_alt = -100.0
+# GPS Setup End <====================
 
 bme680.sea_level_pressure = 1013.25
 temperature_offset = -5
@@ -98,8 +84,14 @@ def cpu_temp():
 #     file.write("time,to,ti,gas,hum,press,alt,accx,accy,accz," +
 #         "gyrox,gyroy,gyroz,magnx,magny,magnz,vbus,ibus,pbus,tc,lat,lon\n")
 if os.stat(TELEMETRY_FILENAME).st_size == 0:
-    telemetry_file.write("time,to,ti,gas,hum,press,alt,accx,accy,accz," +
-        "gyrox,gyroy,gyroz,magnx,magny,magnz,vbus,ibus,pbus,tc\n")
+    telemetry_file.write("date_time," +
+                        "gps_lon,gps_lat,gps_alt,tmp117_temp_out," + 
+                        "bme680_temp_in,bme680_gas,bme680_rel_hum,bme680_press,bme680_alt" +
+                        "icm_acc_x,icm_acc_y,icm_acc_z," +
+                        "icm_gyro_x,icm_gyro_y,icm_gyro_z," +
+                        "icm_magn_x,icm_magn_y,icm_magn_z," +
+                        "ina_bus_vol,ina_bus_cur,ina_bus_pow," +
+                        "temp_cpu\n")
 
 # 3. start wifi hot spot and streaming as a separate process
 #       systemctl enable hostapd 
@@ -107,6 +99,8 @@ if os.stat(TELEMETRY_FILENAME).st_size == 0:
 
 # 4. main loop running with some delay after last iteration
 shut_down = False
+once_in_every_n_lines = 10         # number of telemetry loops between rockblock send
+telemetry_line_counter = 0       # telemetry loop counter
 while True:
     # check for low voltage
     voltage = read_voltage(bus)
@@ -126,48 +120,69 @@ while True:
     now.strftime('%y-%m-%d %H:%M:&S')
     # print(now)
 
+    # get GPS coordinates
+    for new_data in gps_socket:
+        if new_data:
+            data_stream.unpack(new_data)
+            if gps_lon != 'n/a':
+                gps_lon = data_stream.lon
+            if gps_lat != 'n/a':
+                gps_lat = data_stream.lat
+            if gps_alt != 'n/a':
+                gps_alt = data_stream.alt
+        else:
+            break
+
     # get sensor data points
-    # data_points = [tmp117.temperature, bme680.temperature, bme680.gas, 
-    #     bme680.relative_humidity, bme680.pressure, bme680.altitude, 
-    #     *icm.acceleration, *icm.gyro, *icm.magnetic, ina.bus_voltage, 
-    #     ina.current, ina.power,data_stream.lat,data_stream.lon]
-    data_points = [tmp117.temperature, bme680.temperature, bme680.gas, 
-        bme680.relative_humidity, bme680.pressure, bme680.altitude, 
-        *icm.acceleration, *icm.gyro, *icm.magnetic, ina.bus_voltage, 
-        ina.current, ina.power]
+    data_points = [tmp117.temperature, 
+        bme680.temperature, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude, 
+        *icm.acceleration, *icm.gyro, *icm.magnetic, 
+        ina.bus_voltage, ina.current, ina.power]
 
     # truncate to 2 decimal places
     as_string = ','.join([str(round(d, 2)) for d in data_points])
 
     # compose data line
-    line = str(now) + ',' + as_string + ',' + cpu_temp() + '\n'
+    line = str(now) + ',' + \
+        str(gps_lon) + ',' + str(gps_lat) + ',' + str(gps_alt) + ',' + \
+        as_string + ',' + \
+        cpu_temp() + '\n'
     print(len(line))  # 126 w/ tmp117
 
     # append line to telemetry file
     telemetry_file.write(line)
     telemetry_file.flush()
 
-    # time.sleep(30)
-    # rb.text_out = line[:120]  # library limits strings to 120 bytes
-    rb.text_out = line
-    print("Attempting to send telemetry over Iridium...")
-    status = rb.satellite_transfer()
-    
-    # loop as needed
-    retry = 0
-    while status[0] > 8 and retry > 0:
-        time.sleep(10)
-        status = rb.satellite_transfer()
-        print("Retrying", retry, status)
-        retry -= 1
+    if telemetry_line_counter % once_in_every_n_lines == 0:
 
-    if status[0] > 8:
-        print("Unsuccessful. Aborting!")
-    else:
-        print("Success. DONE!")
+        # time.sleep(30)
+        # rb.text_out = line[:120]  # library limits strings to 120 bytes
+        # TODO: Send as data
+        # rb.data_out = line
+        rb.text_out = line   # library hacked (TODO: send as data)
+        print("Attempting to send telemetry over Iridium...")
+        status = rb.satellite_transfer()
+        
+        # loop as needed
+        retry = 2
+        while status[0] > 8 and retry > 0:
+            time.sleep(10)
+            status = rb.satellite_transfer()
+            print("Retrying", retry, status)
+            retry -= 1
+
+        if status[0] > 8:
+            print("Unsuccessful. Aborting!")
+        else:
+            print("Success. DONE!")
+
+    telemetry_line_counter += 1
 
 # close telemetry file
 telemetry_file.close()
+
+# close GPS socket
+gps_socket.close()
 
 # shut down
 if shut_down:
