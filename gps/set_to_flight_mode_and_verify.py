7c32ac6ed3cedf4ab03f679f8e0db9b0919f2f3d
Conlan Cason <conlan.cason9@gmail.com>
2022-02-06 19:27:24 -0700

    Create a function to combine the other functions for ease of use

--- gps/set_to_flight_mode_and_verify.py
+++ gps/set_to_flight_mode_and_verify.py
@@ -1,8 +1,6 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=5)
-
 # command to set module to flight/nav mode
 # header = xB5 x62
 # class = x06
@@ -17,6 +15,13 @@ cmd_navmode = b'\xB5\x62\x06\x24\x24\x00\xFF\xFF\x06\x03\x00\x00\x00\x00\x10\x27
 # command to poll for NAV5
 cmd_poll_navmode = b'\xB5\x62\x06\x24\x00\x00\x2A\x84'
 
+def open_serial(port='/dev/ttyS0', baudrate=9600, timeout=5):
+    """Open a serial port to the GNSS receiver"""
+    
+    global ser
+    ser = serial.Serial(port, baudrate, timeout=timeout)
+
+
 def set_to_flight_mode():
     """Sets the u-blox M8 receiver's dynamic platform model to airborn with < 1g"""
     
@@ -29,7 +34,7 @@ def verify_in_flight_mode():
     """Returns True if the u-blox M8 receiver's dynamic platform model is set to airborne with < 1g"""
     
     if ser.is_open:
-    
+        
         ser.write(cmd_poll_navmode)
         
         # skim through receiver input until we reach the NAV5 confirmation
@@ -59,3 +64,14 @@ def close_serial():
     time.sleep(1)
     
     ser.close()
+
+
+def set_to_flight_mode_and_verify():
+    """Easy function to run everything in the module; Returns the result of verify_in_flight_mode()"""
+
+    open_serial()
+    set_to_flight_mode()
+    is_in_flight_mode = verify_in_flight_mode()
+    close_serial()
+    
+    return is_in_flight_mode
