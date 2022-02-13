fda0ad8ab36644496e629caa03c0c5a194866132
Conlan Cason <conlan.cason9@gmail.com>
2022-02-13 15:41:57 -0700

    Add record_flight_data.py

---
+++ gps/record_flight_data.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def append_new_data(file='flight_data.csv', include_header=False):
+    """Append one line of csv location data to a file
+
+    Keyword arguments:
+    file -- the file where data will be appended; creates a new file if one doesn't already exist (default 'flight_data.csv')
+    include_header -- set to True to append the header fields to the file (default False)
+    """
+    
+    if include_header:
+        header_arg = '1'
+    else:
+        header_arg = '0'
+    
+    # run the gpsd command to get the current location data
+    args = ['gpscsv', '-n', '1', '--header', header_arg]
+    cmd = subprocess.run(args, capture_output=True, text=True)
+    
+    # create the file if it doesn't already exist
+    #f = open(file, 'w')
+    #f.close()
+    
+    # write the command output to the file
+    f = open(file, 'a')
+    f.write(cmd.stdout)
+    f.close()
